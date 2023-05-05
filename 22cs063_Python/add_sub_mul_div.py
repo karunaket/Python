8a44131b66097d393d600102e0d5e1a0fcc2ab5b
karuna ketan <81628692+karunaket@users.noreply.github.com>
2023-05-05 11:40:38 +0530

    Add files via upload

---
+++ 22cs063_Python/add_sub_mul_div.py
@@ -0,0 +1,22 @@
+# a=int(input("Enter 1st no."))
+# b=int(input("Enter 2nd no."))
+
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a/b
+
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+a=float(input("Enter 1st no."))
+b=float(input("Enter 2nd no."))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print(c, d, e, f) #If we want to get output in same line
