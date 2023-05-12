22c55615cb30e8f449d73974e202abe22da83990
karuna ketan <81628692+karunaket@users.noreply.github.com>
2023-05-12 12:08:57 +0530

    Update 13.py

--- Python day-2/13.py
+++ Python day-2/13.py
@@ -3,7 +3,7 @@ b = 3.14
 c = "Hello, World!"
 d = True
 
-print("Value:", a, "Type:", type(a), "ID:", id(a)
+print("Value:", a, "Type:", type(a), "ID:", id(a))
 print("Value:", b, "Type:", type(b), "ID:", id(b))
 print("Value:", c, "Type:", type(c), "ID:", id(c))
 print("Value:", d, "Type:", type(d), "ID:", id(d))
